4c10fe3a63538c9a5934a737b5ef3154b369a8b1
Mauriza Arianne <76801693+Amenonuhoko@users.noreply.github.com>
2023-02-15 12:22:34 +0800

    prototype stage

---
+++ DataStructureWiki/DataStructureWiki.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace DataStructureWiki
+{
+    partial class DataStructureWiki
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.BtnLoad = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataListView = new System.Windows.Forms.ListView();
+            this.dataName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.dataCategory = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.dataStructure = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.dataDefinition = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.categoryTextBox = new System.Windows.Forms.TextBox();
+            this.BtnSave = new System.Windows.Forms.Button();
+            this.BtnAdd = new System.Windows.Forms.Button();
+            this.BtnEdit = new System.Windows.Forms.Button();
+            this.BtnDel = new System.Windows.Forms.Button();
+            this.BtnSearch = new System.Windows.Forms.Button();
+            this.BtnSort = new System.Windows.Forms.Button();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.definitionTextBox = new System.Windows.Forms.TextBox();
+            this.radioButtonLinear = new System.Windows.Forms.RadioButton();
+            this.radioButtonNonLinear = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameTextBox.Location = new System.Drawing.Point(251, 115);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(163, 31);
+            this.nameTextBox.TabIndex = 0;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(542, 70);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(143, 31);
+            this.textBox5.TabIndex = 4;
+            // 
+            // BtnLoad
+            // 
+            this.BtnLoad.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnLoad.Location = new System.Drawing.Point(12, 12);
+            this.BtnLoad.Name = "BtnLoad";
+            this.BtnLoad.Size = new System.Drawing.Size(84, 30);
+            this.BtnLoad.TabIndex = 5;
+            this.BtnLoad.Text = "Load";
+            this.BtnLoad.UseVisualStyleBackColor = true;
+            this.BtnLoad.Click += new System.EventHandler(this.BtnLoad_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 29);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Name";
+            // 
+            // dataListView
+            // 
+            this.dataListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.dataName,
+            this.dataCategory,
+            this.dataStructure,
+            this.dataDefinition});
+            this.dataListView.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dataListView.HideSelection = false;
+            this.dataListView.Location = new System.Drawing.Point(439, 115);
+            this.dataListView.Name = "dataListView";
+            this.dataListView.Size = new System.Drawing.Size(336, 312);
+            this.dataListView.TabIndex = 12;
+            this.dataListView.UseCompatibleStateImageBehavior = false;
+            this.dataListView.View = System.Windows.Forms.View.Details;
+            this.dataListView.Click += new System.EventHandler(this.dataListView_Click);
+            // 
+            // dataName
+            // 
+            this.dataName.Text = "Name";
+            this.dataName.Width = 77;
+            // 
+            // dataCategory
+            // 
+            this.dataCategory.Text = "Category";
+            this.dataCategory.Width = 84;
+            // 
+            // dataStructure
+            // 
+            this.dataStructure.Text = "Structure";
+            this.dataStructure.Width = 79;
+            // 
+            // dataDefinition
+            // 
+            this.dataDefinition.Text = "Definition";
+            this.dataDefinition.Width = 92;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 29);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Category";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 198);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 29);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Structure";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 241);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(114, 29);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Definition";
+            // 
+            // categoryTextBox
+            // 
+            this.categoryTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.categoryTextBox.Location = new System.Drawing.Point(251, 157);
+            this.categoryTextBox.Name = "categoryTextBox";
+            this.categoryTextBox.Size = new System.Drawing.Size(163, 31);
+            this.categoryTextBox.TabIndex = 17;
+            // 
+            // BtnSave
+            // 
+            this.BtnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnSave.Location = new System.Drawing.Point(102, 12);
+            this.BtnSave.Name = "BtnSave";
+            this.BtnSave.Size = new System.Drawing.Size(84, 30);
+            this.BtnSave.TabIndex = 19;
+            this.BtnSave.Text = "Save";
+            this.BtnSave.UseVisualStyleBackColor = true;
+            // 
+            // BtnAdd
+            // 
+            this.BtnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnAdd.Location = new System.Drawing.Point(251, 12);
+            this.BtnAdd.Name = "BtnAdd";
+            this.BtnAdd.Size = new System.Drawing.Size(84, 30);
+            this.BtnAdd.TabIndex = 20;
+            this.BtnAdd.Text = "Add";
+            this.BtnAdd.UseVisualStyleBackColor = true;
+            this.BtnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // BtnEdit
+            // 
+            this.BtnEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnEdit.Location = new System.Drawing.Point(341, 12);
+            this.BtnEdit.Name = "BtnEdit";
+            this.BtnEdit.Size = new System.Drawing.Size(84, 30);
+            this.BtnEdit.TabIndex = 21;
+            this.BtnEdit.Text = "Edit";
+            this.BtnEdit.UseVisualStyleBackColor = true;
+            // 
+            // BtnDel
+            // 
+            this.BtnDel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnDel.Location = new System.Drawing.Point(431, 12);
+            this.BtnDel.Name = "BtnDel";
+            this.BtnDel.Size = new System.Drawing.Size(84, 30);
+            this.BtnDel.TabIndex = 22;
+            this.BtnDel.Text = "Delete";
+            this.BtnDel.UseVisualStyleBackColor = true;
+            // 
+            // BtnSearch
+            // 
+            this.BtnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnSearch.Location = new System.Drawing.Point(691, 70);
+            this.BtnSearch.Name = "BtnSearch";
+            this.BtnSearch.Size = new System.Drawing.Size(84, 31);
+            this.BtnSearch.TabIndex = 23;
+            this.BtnSearch.Text = "Search";
+            this.BtnSearch.UseVisualStyleBackColor = true;
+            // 
+            // BtnSort
+            // 
+            this.BtnSort.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnSort.Location = new System.Drawing.Point(439, 70);
+            this.BtnSort.Name = "BtnSort";
+            this.BtnSort.Size = new System.Drawing.Size(84, 31);
+            this.BtnSort.TabIndex = 24;
+            this.BtnSort.Text = "Sort";
+            this.BtnSort.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 68);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(176, 29);
+            this.label5.TabIndex = 25;
+            this.label5.Text = "Data Structures";
+            // 
+            // definitionTextBox
+            // 
+            this.definitionTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.definitionTextBox.Location = new System.Drawing.Point(251, 241);
+            this.definitionTextBox.Multiline = true;
+            this.definitionTextBox.Name = "definitionTextBox";
+            this.definitionTextBox.Size = new System.Drawing.Size(163, 186);
+            this.definitionTextBox.TabIndex = 26;
+            // 
+            // radioButtonLinear
+            // 
+            this.radioButtonLinear.AutoSize = true;
+            this.radioButtonLinear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButtonLinear.Location = new System.Drawing.Point(251, 198);
+            this.radioButtonLinear.Name = "radioButtonLinear";
+            this.radioButtonLinear.Size = new System.Drawing.Size(71, 24);
+            this.radioButtonLinear.TabIndex = 27;
+            this.radioButtonLinear.TabStop = true;
+            this.radioButtonLinear.Text = "Linear";
+            this.radioButtonLinear.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonNonLinear
+            // 
+            this.radioButtonNonLinear.AutoSize = true;
+            this.radioButtonNonLinear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButtonNonLinear.Location = new System.Drawing.Point(328, 198);
+            this.radioButtonNonLinear.Name = "radioButtonNonLinear";
+            this.radioButtonNonLinear.Size = new System.Drawing.Size(105, 24);
+            this.radioButtonNonLinear.TabIndex = 28;
+            this.radioButtonNonLinear.TabStop = true;
+            this.radioButtonNonLinear.Text = "Non-Linear";
+            this.radioButtonNonLinear.UseVisualStyleBackColor = true;
+            // 
+            // DataStructureWiki
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(791, 448);
+            this.Controls.Add(this.radioButtonNonLinear);
+            this.Controls.Add(this.radioButtonLinear);
+            this.Controls.Add(this.definitionTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.BtnSort);
+            this.Controls.Add(this.BtnSearch);
+            this.Controls.Add(this.BtnDel);
+            this.Controls.Add(this.BtnEdit);
+            this.Controls.Add(this.BtnAdd);
+            this.Controls.Add(this.BtnSave);
+            this.Controls.Add(this.categoryTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataListView);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnLoad);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.nameTextBox);
+            this.Name = "DataStructureWiki";
+            this.Text = "Data Structure Wiki";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button BtnLoad;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView dataListView;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox categoryTextBox;
+        private System.Windows.Forms.Button BtnSave;
+        private System.Windows.Forms.Button BtnAdd;
+        private System.Windows.Forms.Button BtnEdit;
+        private System.Windows.Forms.Button BtnDel;
+        private System.Windows.Forms.Button BtnSearch;
+        private System.Windows.Forms.Button BtnSort;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ColumnHeader dataName;
+        private System.Windows.Forms.ColumnHeader dataCategory;
+        private System.Windows.Forms.ColumnHeader dataStructure;
+        private System.Windows.Forms.ColumnHeader dataDefinition;
+        private System.Windows.Forms.TextBox definitionTextBox;
+        private System.Windows.Forms.RadioButton radioButtonLinear;
+        private System.Windows.Forms.RadioButton radioButtonNonLinear;
+    }
+}
+
---
+++ DataStructureWiki/DataStructureWiki.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataStructureWiki
+{
+    public partial class DataStructureWiki : Form
+    {
+        public DataStructureWiki()
+        {
+            InitializeComponent();
+        }
+
+        #region Variable
+        static int row = 12;
+        static int col = 4;
+        string[,] DataTable = new string[row, col];
+        #endregion
+
+
+        #region Event 
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            Add();
+        }
+        private void BtnLoad_Click(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        // TODO
+        private void dataListView_Click(object sender, EventArgs e)
+        {
+            var index = dataListView.SelectedIndices[0];
+
+            nameTextBox.Text = DataTable[index, 0];
+            categoryTextBox.Text = DataTable[index, 1];
+            structureTextBox.Text = DataTable[index, 2];
+            definitionTextBox.Text = DataTable[index, 3];
+        }
+
+        #endregion
+
+
+        #region Method
+
+        private void DisplayData()
+        {
+            dataListView.Items.Clear();
+
+            for (int i = 0; i < row; i++)
+            {
+                ListViewItem data1 = new ListViewItem(DataTable[i, 0]);
+                data1.SubItems.Add(DataTable[i, 1]);
+                data1.SubItems.Add(DataTable[i, 2]);
+                data1.SubItems.Add(DataTable[i, 3]);
+
+                dataListView.Items.Add(data1);
+            }
+
+        }
+
+        // TODO
+        private void Add()
+        {
+            DataTable[0,0] = nameTextBox.Text;
+            DataTable[0,1] = categoryTextBox.Text;
+            DataTable[0,2] = structureTextBox.Text;
+            DataTable[0,3] = definitionTextBox.Text;
+
+            DisplayData();
+        }
+
+        private void LoadData()
+        {
+
+        }
+
+        #endregion
+
+
+    }
+}
