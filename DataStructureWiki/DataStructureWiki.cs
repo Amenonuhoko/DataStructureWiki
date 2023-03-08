8a7777f446031612ea32ad6b2a38776019389362
Mauriza Arianne <76801693+Amenonuhoko@users.noreply.github.com>
2023-03-08 10:45:03 +0800

    added more comments

--- DataStructureWiki/DataStructureWiki.cs
+++ DataStructureWiki/DataStructureWiki.cs
@@ -11,6 +11,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+// Mauriza Arianne, 08/03/2023
+// A wiki for all data structure types
+
 namespace DataStructureWiki
 {
     public partial class DataStructureWiki : Form
@@ -20,18 +24,22 @@ namespace DataStructureWiki
             InitializeComponent();
         }
 
-        // TODO COMMENTS
         #region Variables
+        // 9.1	Create a global 2D string array, use static variables for the dimensions (row = 4, column = 12),
+
+        // Static should not be changed
         static int row = 12;
         static int col = 4;
+        // Two dimensionals arrays
         string[,] DataTable = new string[row, col];
+        // Default filename for saving and loading
         static string fileName = "definitions.dat";
         int ptr = 0;
         int index = -1;
         #endregion
 
-        // TODO COMMENTS
         #region Events
+        // Events that call the functions
         private void BtnAdd_Click(object sender, EventArgs e)
         {
             Add();
@@ -71,14 +79,16 @@ namespace DataStructureWiki
         }
         #endregion
 
-        // TODO COMMENTS ON METHODS
         #region Method
+
+        // 9.2	Create an ADD button that will store the information from the 4 text boxes into the 2D array,
+        // Adds an entry into the table
         private void Add()
         {
             // Check if fields are empty
             if (String.IsNullOrEmpty(txtBoxName.Text) || String.IsNullOrEmpty(txtBoxCategory.Text) || String.IsNullOrEmpty(txtBoxDefinition.Text))
             {
-                MessageBox.Show("Please Input all the required fields.");
+                MessageBox.Show("Please Input all the required fields.", "Warning", 0, MessageBoxIcon.Warning);
             } else
             {
                 // Make sure the pointer is not out of bounds
@@ -89,52 +99,53 @@ namespace DataStructureWiki
                     DataTable[ptr, 2] = radioButtonLinear.Checked == true ? "Linear" : "Non-Linear";
                     DataTable[ptr, 3] = txtBoxDefinition.Text;
                     ptr++;
-                    
-
                 }
                 // Check if there are more than 12
                 else
                 {
-                    MessageBox.Show("Too many entries");
+                    MessageBox.Show("Too many entries.", "Warning", 0, MessageBoxIcon.Warning);
                 }
                 // Clear fields
                 ClearFields();
-                // Focus
-                txtBoxName.Focus();
                 // Refresh Data
                 DisplayData();
+                // Focus
+                txtBoxName.Focus();
             }
         }
-        // TODO
+
+        //9.7	Write the code for a Binary Search for the Name in the 2D array and display the information in the other textboxes when found, add suitable feedback if the search in not successful and clear the search textbox (do not use any built-in array methods),
+        // Searches for an entry using binary search algorithm
         private void BinarySearch()
         {
+            // Check if textbox is filled
             if (!String.IsNullOrEmpty(txtBoxSearch.Text))
             {
+                // Sort the data and refresh it
                 BubbleSort();
                 DisplayData();
-
+                // Create the target to search
                 string target = txtBoxSearch.Text;
                 int first = 0;
                 int last = row - 1;
                 int position = -1;
-
+                // Entry and exit condition for loop
                 while (first <= last)
                 {
                     int middle = (first + last) / 2;
-                    Debug.WriteLine("first: " + first);
-                    Debug.WriteLine("last: " + last);
-                    Debug.WriteLine(middle);
-
+                    // If target is found
                     if (DataTable[middle, 0] == target)
                     {
+                        // Change position and highlight the entry
                         position = middle;
+                        dataListView.SelectedItems.Clear();
                         dataListView.Items[position].Selected = true;
                         dataListView.Items[position].Focused = true;
-
-                        Debug.WriteLine("here");
-
+                        dataListView.Select();
+                        // Exit out of loop
                         break;
                     }
+                    // Change the middle if entry is higher or lower than the current position
                     else if (String.CompareOrdinal(DataTable[middle, 0], target) < 0)
                     {
                         first = middle + 1;
@@ -144,13 +155,16 @@ namespace DataStructureWiki
                         last = middle - 1;
                     }
                 }
-
+                // If position has not changed then it does not exist
                 if (position == -1)
                 {
                     MessageBox.Show("Does not exist");
                 }
             }
         }
+
+        // 9.6	Write the code for a Bubble Sort method to sort the 2D array by Name ascending, ensure you use a separate swap method that passes the array element to be swapped (do not use any built-in array methods),
+        // Sorts the entries in alphabetic order ascending
         private void BubbleSort()
         {
             // Loop through outer set of data
@@ -160,8 +174,7 @@ namespace DataStructureWiki
                 for (int j = 0; j < row - 1; j++)
                 {
                     // Check if selected item is less value than the one next to it
-                    if (String.CompareOrdinal(DataTable[j, 0], DataTable[j + 1, 0]) > 0)
-                        
+                    if (String.CompareOrdinal(DataTable[j, 0], DataTable[j + 1, 0]) > 0)   
                     {
                         // Swap if true
                         Swap(j);
@@ -169,15 +182,20 @@ namespace DataStructureWiki
                 }
             }
         }
+
+        // 9.5	Create a CLEAR method to clear the four text boxes so a new definition can be added,
+        // Clears the fields
         private void ClearFields()
         {
-            // Clear all fields
             txtBoxName.Clear();
             txtBoxCategory.Clear();
             radioButtonLinear.Checked = false;
             radioButtonNonLinear.Checked = false;
             txtBoxDefinition.Clear();
         }
+
+        // 9.9	Create a method so the user can select a definition (Name) from the ListView and all the information is displayed in the appropriate Textboxes,
+        // Add data into fields from the table using a click
         private void ClickData()
         {
             // Add selected item to index
@@ -194,6 +212,9 @@ namespace DataStructureWiki
                 txtBoxDefinition.Text = DataTable[index, 3];
             }            
         }
+
+        // 9.4	Create a DELETE button that removes all the information from a single entry of the array; the user must be prompted before the final deletion occurs, 
+        // Deletes an entry from the table
         private void Delete()
         {
             // Check if index is in the correct place
@@ -210,11 +231,15 @@ namespace DataStructureWiki
                     DataTable[index, 3] = "zzz";
                     // Reduce pointer
                     ptr--;
+                    // Sort after
                     BubbleSort();
                     DisplayData();
                 }
             }
         }
+
+        // 9.8	Create a display method that will show the following information in a ListView: Name and Category,
+        // Adds the data from the array into the table
         private void DisplayData()
         {
             // Clear list
@@ -235,6 +260,9 @@ namespace DataStructureWiki
                 }
             }
         }
+
+        // 9.3	Create an EDIT button that will allow the user to modify any information from the 4 text boxes into the 2D array,
+        // Edits an entry in the table with current data from the fields
         private void Edit()
         {
             // Error trappings
@@ -249,6 +277,9 @@ namespace DataStructureWiki
                 DisplayData();
             }
         }
+
+        // 9.11	Create a LOAD button that will read the information from a binary file called definitions.dat into the 2D array, ensure the user has the option to select an alternative file. Use a file stream and BinaryReader to complete this task.
+        // Loads a table from a binary file
         private void LoadData()
         {
             // File dialog config
@@ -282,6 +313,9 @@ namespace DataStructureWiki
                 }
             }
         }
+
+        // 9.10	Create a SAVE button so the information from the 2D array can be written into a binary file called definitions.dat which is sorted by Name, ensure the user has the option to select an alternative file. Use a file stream and BinaryWriter to create the file.
+        // Saves a table into a binary file
         private void SaveData()
         {
             // File dialog config
@@ -312,6 +346,8 @@ namespace DataStructureWiki
                 }
             }
         }
+
+        // Swap function for sorting
         private void Swap(int j)
         {
             // Swap for every element in the columns
@@ -322,6 +358,7 @@ namespace DataStructureWiki
                 DataTable[j, x] = temp;
             }
         }
+
         #endregion
 
 
